d5be806591a24b424b7bc1ea3cfaa0919a2e1704
envoffical <edwinnv123@gmail.com>
2026-07-04 00:03:12 +0530

    StudentGradeCalculator.java 2026-07-04

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Marks = ");
+        int marks = scanner.nextInt();
+        char grade;
+        
+        if (marks >= 90 && marks <= 100) {
+            grade = 'A';
+        } else if (marks >= 80 && marks < 90) {
+            grade = 'B';
+        } else if (marks >= 70 && marks < 80) {
+            grade = 'C';
+        } else {
+            grade = 'D';
+        }
+        
+        System.out.println("\nOutput");
+        System.out.println("Grade = " + grade);
+        
+        scanner.close();
+    }
+}
